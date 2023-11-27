b1158463c79638df01e6f1f6c93063519c050891
Jannat Siddique <145537222+JannatSiddique@users.noreply.github.com>
2023-11-27 22:58:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import inquirer from "inquirer";
+const word = await inquirer.prompt([{
+        type: "input",
+        name: "sentence",
+        message: "Type your message here to count the words"
+    }]);
+const sntn = word.sentence.split(" ");
+console.log(`Your sentence word count is ${sntn.length}`);
